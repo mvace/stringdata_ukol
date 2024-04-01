61bf0c7b6edfaf855c1a9674fd1c957a1e7c118e
Marek Vacenovsky <marek.vacenovsky@gmail.com>
2024-04-01 16:06:36 +0200

    Validation adjusted, ERROR list adjusted

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import csv
 from datetime import datetime
 
 DPH_SAZBY = {"zakladni": 0.21, "prvni_snizena": 0.15, "druha_snizena": 0.1}
+FILE_PATH = "nakup.json"
 
 
 def load_data(file_path):
@@ -44,18 +45,19 @@ def validate_data(item, dph_sazby):
                 f"Incorrect data type for {key}. Expected {data_type}, got {type(item[key])}."
             )
 
-    if item.get("dph_sazba") and item.get("dph_sazba") not in dph_sazby:
+    dph_sazba = item.get("dph_sazba")
+    if dph_sazba and dph_sazba not in dph_sazby:
         errors.append(f"Incorect tax rate. Rate {item['dph_sazba']} does not exist.")
 
-    if item.get("mnozstvi") and item.get("mnozstvi") <= 0:
+    mnozstvi = item.get("mnozstvi")
+    if mnozstvi and isinstance(mnozstvi, int) and mnozstvi <= 0:
         errors.append(
-            f"Quantity is {item.get('mnozstvi')}. Quantity cannot be lower or equal to zero."
+            f"Quantity is {mnozstvi}. Quantity cannot be lower or equal to zero."
         )
 
-    if item.get("cena") and item.get("cena") <= 0:
-        errors.append(
-            f"Price is {item.get('cena')}. Price cannot be lower or equal to zero."
-        )
+    cena = item.get("cena")
+    if cena and isinstance(cena, float) and cena <= 0:
+        errors.append(f"Price is {cena}. Price cannot be lower or equal to zero.")
 
     return errors
 
@@ -74,8 +76,8 @@ def calculate_prices(cena_kus_bez_dph, mnozstvi, dph_sazba, dph_sazby):
     Vrátí:
         tuple: Cena za kus s DPH, celková cena bez DPH, celková cena s DPH.
     """
-    cena_kus_s_dph = round(cena_kus_bez_dph * (1 + dph_sazby[dph_sazba]), 1)
-    cena_celkem_bez_dph = round(cena_kus_bez_dph * mnozstvi, 1)
+    cena_kus_s_dph = cena_kus_bez_dph * (1 + dph_sazby[dph_sazba])
+    cena_celkem_bez_dph = cena_kus_bez_dph * mnozstvi
     cena_celkem_s_dph = round(cena_celkem_bez_dph * (1 + dph_sazby[dph_sazba]), 1)
     return cena_kus_s_dph, cena_celkem_bez_dph, cena_celkem_s_dph
 
@@ -119,21 +121,17 @@ def process_data(data, dph_sazby):
                         cena_kus_bez_dph * grocery_dict["items"][nazev]["mnozstvi"]
                     )
                     grocery_dict["items"][nazev]["cena_celkem_s_dph"] = (
-                        cena_celkem_s_dph * grocery_dict["items"][nazev]["mnozstvi"]
+                        cena_kus_s_dph * grocery_dict["items"][nazev]["mnozstvi"]
                     )
                 else:
-                    errors = [
-                        "Items with the same name have different prices or tax rates"
-                    ]
                     err_list = [
                         nazev,
                         mnozstvi,
-                        dph_sazba,
                         cena_kus_bez_dph,
-                        "ERROR",
-                        errors,
+                        ["Items with the same name have different prices or tax rates"],
                     ]
                     grocery_dict["errors"].append(err_list)
+                    print(err_list)
 
             else:
 
@@ -205,12 +203,9 @@ def main():
     """
     Hlavní funkce pro načtení dat, jejich zpracování a export do CSV.
     """
-    file_path = "nakup.json"
-    data = load_data(file_path)
+    data = load_data(FILE_PATH)
     items_dict = process_data(data, DPH_SAZBY)
     export_to_csv(items_dict)
-
-    print(items_dict)
 
 
 if __name__ == "__main__":
